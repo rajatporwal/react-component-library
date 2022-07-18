29631c5c45b2be952e3c9250fd06eb0d85dbaca8
rajatporwal <rajatporwal1483@gmail.com>
2022-07-18 14:25:45 +0530

    code refactor

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,10 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
 
-//NEW
-import { terser } from "rollup-plugin-terser";
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+// NEW
+import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json");
 
@@ -26,22 +24,20 @@ export default [
       },
     ],
     plugins: [
-      // NEW
-      peerDepsExternal(),
-
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      postcss(),
 
       // NEW
-      terser(),
+      postcss(), 
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: [/\.css$/],
+
+    // NEW
+    external: [/\.(css|less|scss)$/],
   },
 ];
