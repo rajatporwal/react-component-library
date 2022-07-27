e389a66e44efb329ddfe85a4aefe4aaf61615ae3
rajatporwal <rajatporwal1483@gmail.com>
2022-07-27 11:59:49 +0530

    global styles component

---
+++ src/components/GlobalStyles/global-styles.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../../styles/main.scss'
+
+const GlobalStyles = (props: any) => {
+    return <div>{props.childred}</div>
+}
+
+export default GlobalStyles
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button";
+export { default as GlobalStyles } from "./GlobalStyles";
 export { default as TypeWriterAnimation } from './typing-effect/typewriter-animation';
 export { default as LoaderCircle } from './loaders/loader-circle';
 export { default as LoaderCircleHorizontal } from './loaders/loader-circle-horizontal';
